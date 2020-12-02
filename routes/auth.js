b000b295e20bdcd17953277c88ef8c40c318b73b
huseind <43807768+huseind@users.noreply.github.com>
2020-12-02 18:26:10 +0100

    deleted unused auth.js route

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express, { Router }  from 'express';
-import user from '../model/user.js';
-import {userServices} from '../services/user.js';
-
-const router = express.Router();
-
-router.post('/login', userServices.createAccount);
-
-export default router;

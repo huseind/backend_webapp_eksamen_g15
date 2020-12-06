ba8e54b4309c9f201efcc318604a9f506b93ffd4
huseind <43807768+huseind@users.noreply.github.com>
2020-12-06 21:44:27 +0100

    fixed filePath for image to localhost

--- services/image.js
+++ services/image.js
@@ -2,13 +2,16 @@ import Image from '../model/image.js';
 import 'dotenv/config';
 
 export const uploadImage = async (data) => {
-    const imagePath = process.env.BASEURL + data.path.replace('public\\', '');
-    const image = new Image({
-        file_path: imagePath, // used to be data.path, this way so article contains right path to image
-        file_mimetype: data.mimetype,
-    });
-    const savedImage = await image.save();
-    return savedImage;
-}
+  const imagePath = `http://${process.env.BASEURL}${data.path.replace(
+    'public\\',
+    ''
+  )}`;
+  const image = new Image({
+    file_path: imagePath, // used to be data.path, this way so article contains right path to image
+    file_mimetype: data.mimetype,
+  });
+  const savedImage = await image.save();
+  return savedImage;
+};
 
 export const getImageById = async (id) => Image.findById(id);

f45692ba5c6e8223e604917b0e8ac5bae3dc72aa
huseind <43807768+huseind@users.noreply.github.com>
2020-12-06 14:09:37 +0100

    type fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,61 +1,102 @@
 import jwt from 'jsonwebtoken';
 import { userServices } from '../services/index.js';
-import { articleController} from '../controllers/index.js';
+import { articleController } from '../controllers/index.js';
 import ErrorHandler from '../utils/errorHandler.js';
 import catchAsync from './catchAsync.js';
 
 export const isAuthenticated = catchAsync(async (req, res, next) => {
   let token;
-  if (req.cookies?.token) { // checking if the req cookie contains token
-    token = req.cookies.token; 
+  if (req.cookies?.token) {
+    // checking if the req cookie contains token
+    token = req.cookies.token;
   }
 
-  if (!token) { // if token is null
+  if (!token) {
+    // if token is null
     return next(new ErrorHandler('You have to log in', 401));
   }
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET); // if there is a token, it is decoded
-  const user = await userServices.getUserById(decoded.id); // getting user by the id 
+  const user = await userServices.getUserById(decoded.id); // getting user by the id
   console.log(user);
 
-  if (!user) { // if the user does not exist
+  if (!user) {
+    // if the user does not exist
     return next(new ErrorHandler('User not found', 404));
   }
   // if the user exists
-  req.user = user;  // setting req user property to the user object
+  req.user = user; // setting req user property to the user object
   next();
 });
 
-export const canViewAllArticles = catchAsync(async(req,res,next)=>{
+export const canViewAllArticles = catchAsync(async (req, res, next) => {
   let token;
-  if (req.cookies?.token) { // checking if the req cookie contains token
-    token = req.cookies.token; 
+  if (req.cookies?.token) {
+    // checking if the req cookie contains token
+    token = req.cookies.token;
   }
 
-  if (!token) { // if token is null, show only public articles
-    return articleController.listPublicArticles(req,res,next);
+  if (!token) {
+    // if token is null, show only public articles
+    return articleController.listPublicArticles(req, res, next);
   }
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET); // if there is a token, it is decoded
-  const user = await userServices.getUserById(decoded.id); // getting user by the id 
+  const user = await userServices.getUserById(decoded.id); // getting user by the id
 
-  //checking user role
-  if(user.role !== 'user' || user.role !== 'admin'){
-    return articleController.listArticles(req,res,next);
+  // checking user role
+  if (user.role !== 'user' || user.role !== 'admin') {
+    return articleController.listArticles(req, res, next);
   }
 
-  if (!user) { // if the user does not exist, show public articles
-    return articleController.listArticles(req,res,next);
+  if (!user) {
+    // if the user does not exist, show public articles
+    return articleController.listArticles(req, res, next);
   }
 
   next();
 });
 
+export const canViewThisArticle = catchAsync(async (req, res, next) => {
+  let token;
+  if (req.cookies?.token) {
+    // checking if the req cookie contains token
+    token = req.cookies.token;
+  }
+
+  if (!token) {
+    // if token is null, show only public articles
+    return next(
+      new ErrorHandler('You have to be logged in to view this article', 400)
+    );
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET); // if there is a token, it is decoded
+  const user = await userServices.getUserById(decoded.id); // getting user by the id
+
+  // checking user role
+  if (user.role !== 'user' || user.role !== 'admin') {
+    return next(
+      new ErrorHandler('You have to be logged in to view this article', 400)
+    );
+  }
+
+  if (!user) {
+    // if the user does not exist, show public articles
+    return next(
+      new ErrorHandler('You have to be logged in to view this article', 400)
+    );
+  }
+
+  next();
+});
 
 // cheking if the user has authority (the right role)
 export const isAuthorized = (...roles) => (req, res, next) => {
   if (!roles.includes(req.user.role)) {
-    return next(new ErrorHandler('You are not authorized for this action', 403));
+    return next(
+      new ErrorHandler('You are not authorized for this action', 403)
+    );
   }
   next();
 };
--- services/article.js
+++ services/article.js
@@ -2,42 +2,43 @@ import Category from '../model/category.js';
 import Article from '../model/article.js';
 import category from '../model/category.js';
 
-////////////////////////////////               CATEGORY               //////////////////////////////
+/// /////////////////////////////               CATEGORY               //////////////////////////////
 
 // listing all categories from db
-export const listCategories = async() => Category.find();
+export const listCategories = async () => Category.find();
 
 // getting a category by id
-export const getCategoryById = async(id) => category.findById(id);
+export const getCategoryById = async (id) => category.findById(id);
 
 // adding a category
-export const createCategory = async(category) => Category.create(category);
+export const createCategory = async (category) => Category.create(category);
 
-
-
-////////////////////////////////               Article               //////////////////////////////
+/// /////////////////////////////               Article               //////////////////////////////
 
 // creating a new article
-export const createArticle = async(article) => Article.create(article);
+export const createArticle = async (article) => Article.create(article);
 
 // getting all articles (for loged in users)
 // populate makes the category field, that is ususally just an id, contain name as well
 // image will contain filepath as well as the id
-export const listArticles = async() => Article.find().populate('category','name').populate('image','file_path');
+export const listArticles = async () =>
+  Article.find().populate('category', 'name').populate('image', 'file_path');
 
-export const listPublicArticles = async() => Article.find({"secret":false}).populate('catagory', 'name').populate('image','file_path');
+export const listPublicArticles = async () =>
+  Article.find({ secret: false })
+    .populate('category', 'name')
+    .populate('image', 'file_path');
 
 export const getArticleById = async (id) => Article.findById(id);
 
-export const editArticle = async (id, editArticle) => 
-    Article.findByIdAndUpdate(id, editArticle, {
-        new: true,
-        runValidators: true,
-        useFindAndModify: false,
-      });
+export const editArticle = async (id, editArticle) =>
+  Article.findByIdAndUpdate(id, editArticle, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
 
 export const deleteArticle = async (id) => {
   const article = await Article.findById(id);
   article.remove();
-}
-
+};
